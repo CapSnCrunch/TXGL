5786cae0c1f00be33ca089af44ae0b8ade468819
Samuel Perales <samuelperales4@gmail.com>
2022-07-11 17:09:00 -0500

    rotate intervals until they work with numpy

--- lambda.py
+++ lambda.py
@@ -1,51 +1,111 @@
+from matplotlib.pyplot import get
 import numpy as np
 from math import isclose
 from classes.intervals import *
 from classes.interval_funcs import *
 
-def P(v, v1, v2):
+def P(v):
     '''Maps RP1 -> R U {infinity}'''
-    a2 = (v[0]**2) * (v2[0]**2 + v2[1]**2) - v2[0]**2
-    a1 = (v[0]**2) * (2*v1[0]*v2[0] + 2*v1[1]*v2[1]) - 2*v1[0]*v2[0]
-    a0 = (v[0]**2) * (v1[0]**2 + v1[1]**2) - v1[0]**2
+    if v[0] == 0:
+        return np.inf
+    return float(v[1] / v[0])
 
-    return float((-a1 + np.sqrt(a1**2 - 4*a0*a2)) / (2*a0))
-
-def Pinv(a, v1, v2):
+def Pinv(a):
     '''Maps R U {infinity} -> RP1'''
     if a == np.inf:
-        return v2
-    return (v1 + a * v2) / np.linalg.norm(v1 + a * v2)
+        return np.array([[1],[0]])
+    return (1 / np.sqrt(1 + a**2)) * np.array([[1],[a]])
 
 def get_lambda(interval, image):
     Ia, Ib = interval.a, interval.b
     Ja, Jb = image.a, image.b
 
-    Ia, Ib = np.transpose(rp1_interval(Ia, Ib))
-    Ja, Jb = np.transpose(rp1_interval(Ja, Jb))
+    u, v = np.transpose(rp1_interval(Ia, Ib))
+    w, z = np.transpose(rp1_interval(Ja, Jb))
 
-    v1 = (Ia+Ib)/2
-    v2 = (Ib-Ia)/2
+    a, b = P(u), P(v)
+    c, d = P(w), P(z)
 
-    c, d = P(Ja,v1,v2), P(Jb,v1,v2)
-    w, z = Pinv(c,v1,v2), Pinv(d,v1,v2)
+    # Rotate until the intervals will work nicely with Numpy
+    for i in range(8):
+        if b < a:
+            Ia = (Ia + np.pi/8) % np.pi
+            Ib = (Ib + np.pi/8) % np.pi
+            Ja = (Ja + np.pi/8) % np.pi
+            Jb = (Jb + np.pi/8) % np.pi
 
-    print(f'w = {w}')
-    print(f'z = {z}')
+            u, v = np.transpose(rp1_interval(Ia, Ib))
+            w, z = np.transpose(rp1_interval(Ja, Jb))
 
-    w0, w1 = w[0], w[1]
-    z0, z1 = z[0], z[1]
+            a, b = P(u), P(v)
+            c, d = P(w), P(z)
 
-    sigma = (((w0+w1)*(z0+z1)) / ((w1-w0)*(z1-z0))) ** (0.25)
-    A = (1/(2*sigma)) * np.array([[sigma**2 + 1, 1 - sigma**2], [1 - sigma**2, 1 + sigma**2]])
-    print(f'det(A) = {np.linalg.det(A)}')
+        else:
+            break
 
+    u, v = Pinv(a), Pinv(b)
+    w, z = Pinv(c), Pinv(d)
+
+    print(f'Ia = {Ia}, Ib = {Ib}, Ja = {Ja}, Jb = {Jb}')
+    print(f'a = {a}, b = {b}, c = {c}, d = {d}')
+    print(f'u = {u}, v = {v}, w = {w}, z = {z}')
+    print()
+
+    # Transforms outside interval to (-1, 1)
+    # alpha = 1 / (np.sqrt(2*(1+a**2)*(1+b**2)) * (b-a))
+    alpha = np.sqrt(1 / (2*(b-a)))
+    A = alpha * np.array([[b-a, 0], [-b-a, 2]])
+
+    # A should be in SL(2,R)
+    print(f'A = {A}')
+    print(f'Det(A) = {np.linalg.det(A)}')
+
+    Au = A @ u 
+    Av = A @ v
     Aw = A @ w
     Az = A @ z
 
-    print(f'P(Aw) = {P(Aw,v1,v2)}, P(Az) = {P(Az,v1,v2)}')
+    # print(f'Au = {Au}')
+    # print(f'Av = {Av}')
+    
+    # These should be -1 and 1
+    print(f'P(Au) = {P(Au)}, P(Av) = {P(Av)}')
+    print()
 
-I = Interval(np.pi/6, np.pi/2-np.pi/8)
+    w0, w1 = Aw[0][0], Aw[1][0]
+    z0, z1 = Az[0][0], Az[1][0]
+
+    sigma = (((w0+w1)*(z0+z1)) / ((w1-w0)*(z1-z0))) ** (0.25)
+    print(f'sigma = {sigma}')
+
+    B = (1/(2*sigma)) * np.array([[sigma**2 + 1, 1 - sigma**2], [1 - sigma**2, 1 + sigma**2]])
+    print(f'B = {B}')
+    print(f'Det(B) = {np.linalg.det(B)}')
+
+    BAu = B @ Au
+    BAv = B @ Av
+    BAw = B @ Aw
+    BAz = B @ Az
+
+    # These should remain the same as before
+    print(f'P(BAu) = {P(BAu)}, P(BAv) = {P(BAv)}')
+
+    # These should negate one another
+    print(f'P(BAw) = {P(BAw)}, P(BAz) = {P(BAz)}')
+    print()
+
+    return 1/abs(P(BAw))
+
+I = Interval(np.pi/6, np.pi/2+np.pi/8)
 J = Interval(np.pi/4, np.pi/3)
 
 print(f'Lambda for I, J = {get_lambda(I, J)}')
+
+# Garbage but holding on to just in case
+# p2 = (w1-w0)*(z0-z1) + (w0-w1)*(z1-z0)
+# p1 = (w1-w0)*(z0+z1) + (w0+w1)*(z0-z1) + (w0-w1)*(z0+z1) + (w0+w1)*(z1-z0)
+# p0 = (w0+w1)*(z0+z1) + (w0+w1)*(z0+z1)
+# p2 = (c-b)*(b-d) + (d-b)*(b-c)
+# p1 = -(c-b)*(a+d) - (b-c)*(b-d) - (b-c)*(b-d) - (a+c)*(d-b)
+# p0 = (b-c)*(a+d) + (a+c)*(b-d)
+# sigma = ((-p1 + (p1**2 - 4*p0*p2)**(1/2)) / (2*p0)) ** (1/2)
